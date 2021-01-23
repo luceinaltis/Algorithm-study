829d618727d16316f5f728db8ab4ea5b2f057cb8
luceinaltis <luceinaltis2020@naver.com>
2021-01-23 12:38:47 +0900

    BOJ9084

---
+++ BOJ9084.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> coins;
+vector<int> cache;
+
+int main()
+{
+    int t;
+
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        int m;
+
+        cin >> n;
+
+        coins.assign(n, 0);
+        cache.assign(10001, 0);
+        cache[0] = 1;
+
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> coins[i];
+        }
+
+        cin >> m;
+
+        int ans = 0;
+        for (int i = 0; i < coins.size(); ++i)
+        {
+            for (int j = coins[i]; j <= m; ++j)
+            {
+                cache[j] += cache[j - coins[i]];
+            }
+        }
+
+        cout << cache[m] << '\n';
+    }
+
+    return 0;
+}
