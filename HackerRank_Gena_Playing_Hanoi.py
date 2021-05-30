64a5b6dd541fca9bd0e072c4841e0510dd71e0c6
luceinaltis <luceinaltis2020@naver.com>
2021-05-30 16:50:25 +0900

    Gena Playing Hanoi

---
+++ HackerRank_Gena_Playing_Hanoi.py
@@ -0,0 +1,20 @@
+def Hanoi(posts, pos):
+    if pos == -1:
+        return 0
+
+    if posts[pos] != 1:
+        return 1 + 2 * Hanoi(posts, pos-1)
+    else:
+        return 0 + 2 * Hanoi(posts, pos-1)
+
+
+def main():
+    n = int(input())
+    posts = list(map(int, input().split()))
+
+    print(Hanoi(posts, len(posts) - 1))
+
+    return 0
+
+
+main()
